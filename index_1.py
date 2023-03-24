55639d1c2fc10bd2b5b126534d329d52cb4a4be7
Julian Solano <juliansoza19@gmail.com>
2023-03-24 14:56:21 -0500

    Se comneta el codigo

--- index_1.py
+++ index_1.py
@@ -3,136 +3,152 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from scipy import integrate
 from funtions_1 import *
-                                 
+
+#----------declaración de variables iniciales ------------------------------------------------------------------------------------
+df = pd.read_json("vigas_data.json")            # Base de datos de tipos de vigas
 Qys = []                                        # Vector para los primeros momentos de area 
 coordenadas_p = []                              # Vector para las coordenadas de cargas puntuales
 v_cortantes = []                                # Vector para la los cortantes 
-eitetha = []                                    #Vector para las pendientes ssssss
-tetha = []                          
-eideflexiones = []
-elm = 1000
-delt = 0.001
+eitetha = []                                    # Vector para la integracion numérica para las pendientes (E*I*Tetha)
+tetha = []                                      # Vector para las pendientes
+eideflexiones = []                              # Vector para la integracion numérica para las deflexiones (E*I*Tetha)
 
-print('Ingrese la longitud de la viga:')
-L = float(input(""))
-LR = np.arange(0 ,L ,delt)
-Lg = int(L+1)                                   #Longitud de la barra para graficar
+elm = 1000                                      # numero de elemnetos diferenciales 
+delt = 0.001                                    # paso del método numerico
 
+print('Ingrese la longitud de la viga:')        # hace parte del menú de usuario
+L = float(input(""))                            # almacena la longitud de la viga
+LR = np.arange(0 ,L ,delt)                      # crea el eje x del sistema de referencia (vector de posicion)
+Lg = int(L+1)                                   # Longitud de la barra para graficar
 
-Vpc = np.repeat(float(0),len(LR))               #Se almacenan las cargas en cada punto
-momentums = np.repeat(float(0),len(LR))         #Se almacenan los momentos en cada punto 
+Vpc = np.repeat(float(0),len(LR))               # Se almacenan las cargas en cada punto
+momentums = np.repeat(float(0),len(LR))         # Se almacenan los momentos en cada punto 
 
-Aa, Ab = apoyos()                               #Se ingresan los apoyos
+Aa, Ab = apoyos()                               # Se ingresan los apoyos
 
+# ----------- Menu de ususario ---------------------------------------------------------------------------------------------------------
+i = 0                                               # iterador
+while (i==0):                                       # ciclo de ingreso de carga
+    print('Escoja la carga que quiere aplicar')     # hace parte del menu de usuario
+    print('1. Carga puntual')                       # "             "
+    print('2. Carga distribuida')                   # "             "
+    n = input()                                     # variable de switch 
 
+#Cargas puntuales ------------------------------------------------------------------------------------------------------
+    if n == '1':
+        Qy , cordenada = Cargas_puntuales_f(Vpc)    # calcula el momento de area e ingresa la cordenada de la carga 
+        Qys.append(Qy)                              # almacena el momento de area
+        coordenadas_p.append(cordenada)             # almacena la carga
 
-#Menu de ususario
-i = 0
-while (i==0):
-    print('Escoja la carga que quiere aplicar')
-    print('1. Carga puntual')
-    print('2. Carga distribuida')
-    n = int(input())
+#Cargas distribuidas ---------------------------------------------------------------------------------------------------
+    elif n == '2':
+        Qy , area = Cargas_distribuidas_f(elm , LR , Vpc) # calcula el momento de area y calcula la carga equivalente
+        Qys.append(Qy)                                    # almacena el momento de area
+# ----
+    else:                                               # hace parte del menu de usuario
+        print('Por favor ingrese un numero valido')     # "             "
 
-#Cargas puntuales y distribuidas------------------------------------------------------------------------------------------------------
-    if n == 1:
-       
-        Qy , cordenada = Cargas_puntuales_f(Vpc)
-        Qys.append(Qy)
-        coordenadas_p.append(cordenada)
-
-    elif n == 2:
-       
-        Qy , area = Cargas_distribuidas_f(elm , LR , Vpc)
-        Qys.append(Qy)
-
-    print('Desea agregar otra carga?')
-    print('1. Si  |  2. No')
-    r = input()
-    if (r=='1'):
-        i = 0
-    else:
-        i = 1
+    print('Desea agregar otra carga?')                  # "             "
+    print('1. Si  |  2. No')                            # "             "
+    r = input()                                         # variable de switch 
+    if (r=='1'):                                        # se sale del ciclo de ingreso de carga
+        i = 0                                           # "             "
+    else:                                               # repite el ciclo de ingreso de carga
+        i = 1                                           # "             "
 
 momentums = Momentos_f(elm , momentums)
 
-#reaccciones--------------------------------------------------------------------------------------------------------------------------
-xel = Xel_total(Qys,Vpc)
+print('Seleccione el numero del material de la viga: ')
+print('1. Acero')
+print('2. Aluminio')
+m = input()
 
-R2 = (sum(Vpc)*(xel-Aa) - sum(momentums)) / (Ab-Aa)
-R1 = sum(Vpc) - R2
+print('Seleccione el numero del tipo de perfil de la viga: ')
+print('1. W')
+p = input()
 
+if (p == '1'):
+    print(df.iloc[:, 0:3])
+    print('Seleccione la designación de la viga: ')
+    print(df.iloc[1:, 0:3])
+    d = int(input())
+    E_i = (df.iloc[d,2])*(df.iloc[d,3])
+else:
+    print('Por favor ingrese un numero valido')
+
+#---------------- Analisis -----------------------------------------------------------------------------------------------------------
+
+#reaccciones----------------------------------------------------------------------------------------
+xel = Xel_total(Qys,Vpc)                                #calculo del centroide la distribución de cargas
+
+R2 = (sum(Vpc)*(xel-Aa) - sum(momentums)) / (Ab-Aa)     # calculo de la reaccion en el segundo apoyo
+R1 = sum(Vpc) - R2                                      # calculo de la reaccion en el primer apoyo
+
+#Verificacion
 print("x de elemento: {}".format(xel))
 print("La reaccion en el primer apoyo es: {}".format(R1))
 print("La reaccion en el segundo apoyo es: {}".format(R2))
 print("La carga total es: ", sum(Vpc))
+#
 
-# Las reacciones entran en Vpc-------------------------------------------------------------------------------------------------------
-Vpc = -Vpc
-if Aa == 0:
-    Vpc[0] += R1
-    Vpc[(int(Ab*elm))-1] += R2
-else:
-    Vpc[(int(Aa*elm))-1] += R1
-    Vpc[(int(Ab*elm))-1] += R2
+Vpc = -Vpc                          # Crea el eje y del sistema, se direccionan las cargas
+if Aa == 0:                         # si el primer apoyo se encuentra en el origen 
+    Vpc[0] += R1                    # se agrega la reaccion por medio de superposicion
+    Vpc[(int(Ab*elm))-1] += R2      # se agrega la reaccion por medio de superposicion
+else:                               # si el primer apoyo se encuentra a un distancia diferente de 0 del origen 
+    Vpc[(int(Aa*elm))-1] += R1      # se agrega la reaccion por medio de superposicion
+    Vpc[(int(Ab*elm))-1] += R2      # se agrega la reaccion por medio de superposicion
 
-#cortantes y flectores----------------------------------------------------------------------------------------------------------------
+#Estatica: fuerzas cortantes y momentos flectores ----------------------------------------------------------------------------------------------------------------
 
-v_cortantes = integrar_num(v_cortantes , Vpc,tetha)
+v_cortantes = integrar_num(v_cortantes , Vpc,tetha)     # calculo de las fuerzas cortantes
 
+# calculo de los momentos flectores ------------
+m_flectores = np.repeat(float(0),len(Vpc))              # se crea un vector para los momentos cortantes, inicialmente iguales a 0          
 
-m_flectores = np.repeat(float(0),len(Vpc))
-
-for t in range(len(Vpc)-1):
-    if(momentums[t]<0):
-        m_flectores[t] -= momentums[t] #incrementando el valor de M en dicho punto en una cantidad igual a la magnitud del par, si este último tiene un sentido a favor del novimiento de las manecillas del reloj
+for t in range(len(Vpc)-1):                             # calculo de los momentos flectores por medio de integración numérica
+    if(momentums[t]<0):                                 # ingreso de los momentos puntuales por medio de superposicion
+        m_flectores[t] -= momentums[t]                  # se incrementa el valor del momento en dicho punto en una cantidad igual a la magnitud del par, si este último tiene un sentido a favor del movimiento de las manecillas del reloj
     else:        
-        m_flectores[t] -= momentums[t] 
-    m_flectores[t+1] = (m_flectores[t] + (v_cortantes[t+1]/elm))
+        m_flectores[t] -= momentums[t]                  # se disminuye el valor del momento en dicho punto en una cantidad igual a la magnitud del par, si este último tiene un sentido en contra del movimiento de las manecillas del reloj
+    
+    m_flectores[t+1] = (m_flectores[t] + (v_cortantes[t+1]/elm)) # integración numérica, de divide el valor de la fuerza cortante por mil debido a la discretizacion
 
-#resistencia--------------------------------------------------------------------------------------------------------------------------
+#Resistencia --------------------------------------------------------------------------------------------------------------------------
 
-df = pd.read_json("vigas_data.json")
-print(df)
-print(df.iloc[:, 0:3])
-print(df.iloc[1,2])
+#----- pendientes -------------------------------------------------------------
+eitetha = integrar_num(eitetha , m_flectores, tetha)                        # Integración numérica de los momentos flectores sin resolver el problema de valor inicial
 
-#pendientes---------------------------------------------------------------------------------------------------------------------------
-eitetha = integrar_num(eitetha , m_flectores, tetha)
+#solución del P.V.I teniendo en cuenta las condiciones de frontera y por medio de los teoremas de los momentos de area, se utiliza la región comprendida entre los dos apoyos -----
 
-if Aa == 0:
+if Aa == 0:                                                                 # seleccion de los momentos flectores comprendidos entre los dos apoyos
     vector = m_flectores[0:int((Ab*elm))]
 else:
     vector = m_flectores[int((Aa*elm)):int((Ab*elm))]
 
-print(m_flectores)
-print(vector)    
-
-xvec = np.flip(np.arange(0, Ab-Aa,delt))
-primermomento = integrate.trapz(np.multiply(xvec, vector*(1/(2000*2070000000000)), xvec))
-pendi = -primermomento/(Ab-Aa)
+# se calcula la desviacion tangencial del segundo apoyo con respecto al primer apoyo, aplicando el teorema del segundo momento de área con respecto a un eje vertical que pasa por el segundo apoyo
+xvec = np.flip(np.arange(0, Ab-Aa,delt))                                    # Definicion del eje de cordenadas con respecto al segundo apoyo
+des_tan = integrate.trapz(np.multiply(xvec, vector*(1/(E_i)), xvec))        # calculo de la desviacion tangencial por medio del segundo momento del area
+theta_A = -des_tan/(Ab-Aa)                                                  # pendiente en el primer apoyo, cociente entre la desviacion tangencial del segundo apoyo con respecto al segundo, y la destancia entre estos (se utiliza la definicion de pendiente)
 ##
-if Aa == 0:
-    c1 = np.repeat(2000*2070000000000*pendi-eitetha[0],len(eitetha))
+
+#calculo de la constante c de integración utilizando las condiciones de frontera
+if Aa == 0: 
+    c1 = np.repeat(E_i*theta_A-eitetha[0],len(eitetha))
 else:
-    c1 = np.repeat(2000*2070000000000*pendi-eitetha[int((Aa*elm))-1],len(eitetha))
+    c1 = np.repeat(E_i*theta_A-eitetha[int((Aa*elm))-1],len(eitetha))
 ##
-print(c1[0])
-tetha = (eitetha+c1)*(1/(2000*2070000000000))/elm
-#hacerlo para las tres secciones 
 
-#deflexiones--------------------------------------------------------------------------------------------------------------------------
+tetha = (eitetha+c1)*(1/(E_i))/elm                                          # calculo de las pendientes utilizando la ecuacion matemática
 
-eideflexiones = integrar_num(eideflexiones, Vpc, tetha)
+#deflexiones-------------------------------------------------------------
+
+eideflexiones = integrar_num(eideflexiones, Vpc, tetha)                     # Integración numérica de las pendientes sin resolver el problema de valor inicial
 
 c2 = np.repeat(-eideflexiones[int((Ab*elm))-1],len(eideflexiones))  
 
 deflexiones = (eideflexiones+c2)/elm
 
-
-
-print(c2[0])
-print(deflexiones)
 
 #Grafico de cargas--------------------------------------------------------------------------------------------------------------------
 print("prueba_Sync_3." , sum(Vpc))
