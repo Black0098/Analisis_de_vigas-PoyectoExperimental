5cdb499be8a74e93ed4a448c114e2f7febf45795
karol-p07 <penakarol07@gmail.com>
2023-04-08 12:29:10 -0500

    Graficas_Completas

--- index_1.py
+++ index_1.py
@@ -8,7 +8,7 @@ from matplotlib.patches import Rectangle
 from matplotlib.patches import Polygon
 
 #----------declaración de variables iniciales ------------------------------------------------------------------------------------
-df = pd.read_json("vigas_data.json")            # Base de datos de tipos de vigas
+df = pd.read_json("vigas_data.json")            # Base de datos de types de vigas
 Qys = []                                        # Vector para los primeros momentos de area 
 coordenadas_p = []                              # Vector para las coordenadas de cargas puntuales
 v_cortantes = []                                # Vector para la los cortantes 
@@ -24,8 +24,8 @@ coordenadas_m = []                              #Vector que almacena las coorden
 elm = 1000
 delt = 0.001
 
-print('Escoja el tipo de viga: \n 1. Simplemente apoya\n 2. Empotrada a la izaquierda\n 3. Empotrada a la derecha')
-tipo = input()
+print('Escoja el type de viga: \n 1. Simplemente apoya\n 2. Empotrada a la izaquierda\n 3. Empotrada a la derecha')
+type = input()
 
 print('Ingrese en que sistema va a trabajar')
 print("1. SI    |   2. Ingles")
@@ -39,7 +39,7 @@ Lg = int(L+1)                                   # Longitud de la barra para graf
 Vpc = np.repeat(float(0),len(LR))               # Se almacenan las cargas en cada punto
 momentums = np.repeat(float(0),len(LR))         # Se almacenan los momentos en cada punto 
 
-if (tipo == '1'):
+if (type == '1'):
     Aa, Ab = apoyos()                               # Se ingresan los apoyos
 
 # ----------- Menu de ususario ---------------------------------------------------------------------------------------------------------
@@ -84,7 +84,7 @@ print('1. Acero')
 print('2. Aluminio')
 m = input()
 
-print('Seleccione el numero del tipo de perfil de la viga: ')
+print('Seleccione el numero del type de perfil de la viga: ')
 print('1. W')
 p = input()
 
@@ -102,7 +102,7 @@ else:
 #reaccciones----------------------------------------------------------------------------------------
 xel = Xel_total(Qys,Vpc)                                #calculo del centroide la distribución de cargas
 
-if (tipo == '1'):
+if (type == '1'):
     R2 = (sum(Vpc)*(xel-Aa) - sum(momentums)) / (Ab-Aa)     # calculo de la reaccion en el segundo apoyo
     R1 = sum(Vpc) - R2                                      # calculo de la reaccion en el primer apoyo
     Vpc = -Vpc                          # Crea el eje y del sistema, se direccionan las cargas
@@ -120,7 +120,7 @@ if (tipo == '1'):
     print("La carga total es: ", sum(Vpc))
     #
 
-elif (tipo == '2'):
+elif (type == '2'):
     R = sum(Vpc)
     M_A = sum(Vpc)*(xel) - sum(momentums)
     Vpc = -Vpc
@@ -129,7 +129,7 @@ elif (tipo == '2'):
     print("La reaccion en A es: {}".format(R))
     print("El momento en A es: {}".format(M_A))
 
-elif (tipo == '3'):
+elif (type == '3'):
     R = sum(Vpc)
     M_A = sum(Vpc)*(L-xel) - sum(momentums)
     Vpc = -Vpc
@@ -159,7 +159,7 @@ for t in range(len(Vpc)-1):                             # calculo de los momento
 #Pen_refentes---------------------------------------------------------------------------------------------------------------------------
 eitetha = integrar_num(eitetha , m_flectores)
 
-if (tipo == '1'):
+if (type == '1'):
     if Aa == 0:
         vector_M = m_flectores[0:int((Ab*elm))]               #almacena momentos necesarios para calcular la tangente de referencia (se encuentran entre los dos apoyos)
     else:
@@ -183,7 +183,7 @@ if (tipo == '1'):
     c2 = np.repeat(-eideflexiones[int((Ab*elm))-1],len(eideflexiones))
     deflexiones = (eideflexiones+c2)/elm
 
-elif (tipo == '2'):
+elif (type == '2'):
     vector_M = m_flectores[0:int(L*elm)]
     xvec = np.flip(np.arange(0, L, delt))
     Pen_ref = integrate.trapz(vector_M*(1/(E_i)), xvec)
@@ -195,7 +195,7 @@ elif (tipo == '2'):
     deflexiones = (eideflexiones+c2)/elm
     print("La deflexion en L es: {}".format(deflexiones[int(L*elm)-1]))
 
-elif (tipo == '3'):
+elif (type == '3'):
     vector_M = m_flectores[0:int(L*elm)]
     xvec = np.flip(np.arange(0, L, delt))
     Pen_ref = integrate.trapz(vector_M*(1/(E_i)), xvec)
@@ -209,7 +209,7 @@ elif (tipo == '3'):
 
 #Grafico de cargas--------------------------------------------------------------------------------------------------------------------
 
-if (tipo == '1'):
+if (type == '1'):
     # Magnitud del vector_M
     x = 0
     y = -2
@@ -247,13 +247,45 @@ if (tipo == '1'):
         ax.plot(x, yE_g[i](x), label=f'Function {i+1}')
         ax.fill_between(x, yE_g[i](x), 0, where = yE_g[i](x)>0, interpolate = True, alpha=0.2) #Rellena la grafica
 
-elif (tipo == '2'):
+elif (type == '2'):
     # Magnitud del vector_M
     x = 0
     y = -2
 
     viga = Rectangle(xy = (0,0), height = -0.3, width = Lg, edgecolor='lightslategray', facecolor='lightslategray')
     empotrada = Rectangle(xy=(0,-(Lg/6)), height = Lg, width= -0.2, edgecolor='lightslategray', facecolor='lightslategray')
+    fig, ax = plt.subplots()
+    
+    ax.add_patch(viga)
+    ax.add_patch(empotrada)
+    
+    #Cargas puntuales
+    for m in range(len(coordenadas_p)):
+        if (coordenadas_p[m]>=0):
+         ax.quiver(coordenadas_p[m], 2, x, y, scale_units='xy', scale=1, color = "g")
+
+    #Cargas distribuidas
+    for i in range(len(y2_g)):
+        if "x" not in y2_g[i]:                                             # Si la función en una constante
+            exec(f"j{i} = np.repeat(int(y2_g[i]),elm)")                    # crea variables que van aumentando el numero en funcion de las F_Constantes, cada variable se repite elm_veces
+            y2_g[i] = f"j{i}"                                              # Se añade la repeticion en la posicion por cada F_Constante
+        
+    for i in y2_g:
+        func = lambda x: eval(y2_g[i])                                      #Evalua las funciones
+        yE_g.append(func)                                                   #Se agregan al vector de funciones evaluadas
+
+    for i in range(len(yE_g)):
+        x = np.linspace(xa_g[i], xb_g[i], elm)                                    
+        ax.plot(x, yE_g[i](x), label=f'Function {i+1}')
+        ax.fill_between(x, yE_g[i](x), 0, where = yE_g[i](x)>0, interpolate = True, alpha=0.2) #Rellena la grafica
+
+elif (type == '3'):
+    # Magnitud del vector_M
+    x = 0
+    y = -2
+
+    viga = Rectangle(xy = (0,0), height = -0.3, width = Lg, edgecolor='lightslategray', facecolor='lightslategray')
+    empotrada = Rectangle(xy=(Lg,-(Lg/6)), height = Lg, width= -0.2, edgecolor='lightslategray', facecolor='lightslategray')
     fig, ax = plt.subplots()
     
     ax.add_patch(viga)
